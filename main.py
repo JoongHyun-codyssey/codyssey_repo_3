8809a08faffca2b0c539d557e16d956dc42e8912
JoongHyun-codyssey <dkswndgus0506@gmail.com>
2026-08-07 13:54:44 +0900

    feat: MAC 계산 함수 구현

---
+++ main.py
@@ -0,0 +1,42 @@
+def calculate_mac(pattern, filter_):
+    score = 0
+
+    n = len(pattern)
+    if n != len(filter_):
+        raise ValueError("행 수가 맞지 않습니다.")
+
+    for i in range(n):
+        if n != len(pattern[i]) or n != len(filter_[i]):
+            raise ValueError("열 수가 맞지 않습니다.")
+        for j in range(len(pattern[i])):
+            score += pattern[i][j] * filter_[i][j]
+
+    return float(score)
+
+def main():
+    cross = [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ]
+
+    x_pattern = [
+        [1, 0, 1],
+        [0, 1, 0],
+        [1, 0, 1],
+    ]
+
+    x_filter = [
+        [1, 0, 1],
+        [0, 1, 0],
+        [1, 0, 1],
+    ]
+
+    x_result = calculate_mac(x_pattern, x_filter)
+    cross_result = calculate_mac(cross, x_filter)
+    print(x_result)
+    print(cross_result)
+
+
+if __name__ == "__main__":
+    main()
